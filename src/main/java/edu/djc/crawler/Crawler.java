05f188f9bab449702fb280b62d223b785bf37eab
Jiachen Dai <daijiachen97@163.com>
2021-04-09 20:47:09 +0100

    try again

--- src/main/java/edu/djc/crawler/Crawler.java
+++ src/main/java/edu/djc/crawler/Crawler.java
@@ -9,6 +9,7 @@ public class Crawler implements Callable<Result> {
     public Crawler(String url) {
         this.url = url;
         System.out.println("ok");
+        System.out.println("ok");
     }
 
     @Override
